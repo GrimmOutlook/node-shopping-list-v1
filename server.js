ce5cd1734ea03f0642d1079ff83fc00ace2a4d71
GrimmOutlook <grimmoutlook87@gmail.com>
2017-06-05 20:01:17 -0400

    Added recipes GET endpoint.

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-
 const express = require('express');
 const router = express.Router();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
-const {ShoppingList} = require('./models');
+const {ShoppingList, Recipes} = require('./models');
 
 const jsonParser = bodyParser.json();
 const app = express();
@@ -18,10 +17,17 @@ ShoppingList.create('beans', 2);
 ShoppingList.create('tomatoes', 3);
 ShoppingList.create('peppers', 4);
 
+Recipes.create('chili', ['tomato puree', 'ground beef', 'garlic', 'Dave\'s Insanity Sauce']);
+Recipes.create('disaster', ['stick of dynamite', 'ATV', 'thin ice', 'poor judgment']);
+
 // when the root of this router is called with GET, return
 // all current ShoppingList items
 app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
+});
+
+app.get('/recipes', (req, res) => {
+  res.json(Recipes.get());
 });
 
 app.listen(process.env.PORT || 8080, () => {
